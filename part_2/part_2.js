b21797ffd827a320e6b9d0c6a59c2c372869f5ac
(5)Inas Abu_Hameidan <70091431+inas-mohammad@users.noreply.github.com>
2021-01-01 22:07:24 +0200

    Update part_2.js

--- part_2/part_2.js
+++ part_2/part_2.js
@@ -1,40 +1,46 @@
 /*
 
+/*
+
 PART 2 [SQL Queries]:
 
 Type the correct SQL query below every question:
 
 Q1) What is the query for displaying all the databases?
-
+SHOW DATABASES;
 
 Q2) What is the query for using a specific database?
 
-
+use database_name;
 Q3) What is the query for creating a new database called `meraki`?
+CREAT DATABASE meraki ;
 
 
 Q4) What is the query for deleting a database called `meraki`?
 
-
+DROP DATABASE meraki ;
 Q5) What is the query for displaying all the tables for a database called `meraki`?
 
 
 Q6) What is the query for displaying the columns of a table called `users`?
-
+select * from users ;
 
 Q7) What is the query to insert information into a table called `users` that has four columns (name, email, password, age)?
-
+INSERT INTO users 
+VALUES (name ,email,password,age);
 
 Q8) What is the query to update the `name` of a user that has `info@meraki-academy.org` as an email?
-
+UPDATE users 
+SET name ="kfjjfi"
+WHERE email =`info@meraki-academy.org`;
 
 Q9) What is the query to delete a user that has `info@meraki-academy.org` as an email?
-
+DELET FROM users WHERE email=`info@meraki-academy.org`;
 
 Q10) What is the query to perform a left join on two tables (users, posts) depending on the user_id? (user_id is a FK in `posts` table)
 
 
 Q11) What is the query to get all the users that their age ranges from 20-30 from the `users` table?
-
-
+SELECT * from users 
+ WHERE  age >20 AND  age <30;
 */
